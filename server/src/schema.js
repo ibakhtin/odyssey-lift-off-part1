a085f9f32cd9d3b1a7266e4cd3768088efd8945c
Igor Bakhtin <ibakhtin@ibakhtin.expert>
2021-08-06 22:45:19 +0300

    Update index.js

--- server/src/schema.js
+++ server/src/schema.js
@@ -1,6 +1,27 @@
 const {gql} = require('apollo-server');
 
 const typeDefs = gql`
+  type Query {
+    "Get tracks array for homepage grid"
+    tracksForHome: [Track!]!
+  }
+
   # Schema definitions go here
+  type Track {
+    id: ID!
+    title: String!
+    author: Author!
+    thumbnail: String
+    length: Int
+    moduleCount: Int
+  }
+
+  "Author of a complete Track or a Module"
+  type Author {
+    id: ID!
+    name: String!
+    photo: String
+  }
 `;
+
 module.exports = typeDefs;
